328a28f90359044f4d46851058e2a7ddd7433a82
Bartosz Stalewski <bartosz.stalewski@codilime.com>
2015-05-12 14:26:47 +0200

    Add example project without scala features

---
+++ example/gs-openspaces-scala-example/common/src/main/scala/com/gigaspaces/common/SData.scala
@@ -0,0 +1,49 @@
+package com.gigaspaces.common
+
+import org.openspaces.scala.core.aliases.annotation._
+
+import scala.beans.{BooleanBeanProperty, BeanProperty}
+
+
+case class SData(
+  @BeanProperty @SpaceId(autoGenerate = true) var id: String = null,
+  @BeanProperty @SpaceRouting @SpaceProperty(nullValue = "-1") var `type`: Long = -1,
+  @BeanProperty var rawData: String = null,
+  @BeanProperty var data: String = null,
+  @BooleanBeanProperty var processed: Boolean = false) {
+
+  def this(`type`: Long, rawData: String) = this(null, `type`, rawData, null, false)
+
+  override def toString: String = {
+    return "id[" + id + "] type[" + `type` + "] rawData[" + rawData + "] data[" + data + "] processed[" + processed + "]"
+  }
+}
+
+
+
+
+case class ImmutableData @SpaceClassConstructor() (
+  @BeanProperty
+  @SpaceId(autoGenerate = true)
+  id: String = null,
+
+  @BeanProperty
+  @SpaceRouting
+  @SpaceProperty(nullValue = "-1")
+  `type`: Long = -1,
+
+  @BeanProperty
+  rawData: String = null,
+
+  @BeanProperty
+  data: String = null,
+
+  @BooleanBeanProperty
+  processed: Boolean = false) {
+
+  def this(`type`: Long, rawData: String) = this(null, `type`, rawData, null, false)
+
+  override def toString: String = {
+    return "id[" + id + "] type[" + `type` + "] rawData[" + rawData + "] data[" + data + "] processed[" + processed + "]"
+  }
+}
