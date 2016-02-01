f53ba4b56a4602f139446825c874d740817e55db
Niv Ingberg <niv@gigaspaces.com>
2016-02-01 12:14:12 +0200

    GS-12733 - moved scala jars to scala scripts

--- openspaces-scala/tools/scala/shell.bat
+++ openspaces-scala/tools/scala/shell.bat
@@ -1,6 +1,6 @@
 @call "%~dp0\..\..\bin\setenv.bat"
 
-set REPL_CLASSPATH=-cp %GS_JARS%;%SPRING_JARS%;%SIGAR_JARS%;%SCALA_JARS%;%GS_SCALA_JARS%
+set REPL_CLASSPATH=-cp %GS_JARS%;%SPRING_JARS%;%SIGAR_JARS%;"%XAP_HOME%\lib\platform\scala\lib\*;";"%XAP_HOME%\lib\platform\scala\*;"
 set REPL_JAVA_OPTS=%JAVA_OPTIONS% %RMI_OPTIONS% %XAP_OPTIONS%
 set REPL_MAIN_CLASS=org.openspaces.scala.repl.GigaSpacesScalaRepl
 set REPL_COMPILER_OPTS=-usejavacp -Yrepl-sync
--- openspaces-scala/tools/scala/shell.sh
+++ openspaces-scala/tools/scala/shell.sh
@@ -1,7 +1,7 @@
 export JSHOMEDIR=`dirname $0`/../..
 . `dirname $0`/../../bin/setenv.sh
 
-REPL_CLASSPATH="-cp $GS_JARS:$SPRING_JARS:$SIGAR_JARS:$SCALA_JARS:$GS_SCALA_JARS"
+REPL_CLASSPATH="-cp $GS_JARS:$SPRING_JARS:$SIGAR_JARS:"${XAP_HOME}"/lib/platform/scala/lib/*:"${XAP_HOME}"/lib/platform/scala/*"
 REPL_JAVA_OPTS="$JAVA_OPTIONS $RMI_OPTIONS $XAP_OPTIONS"
 REPL_MAIN_CLASS="org.openspaces.scala.repl.GigaSpacesScalaRepl"
 REPL_COMPILER_OPTS="-usejavacp -Yrepl-sync"
