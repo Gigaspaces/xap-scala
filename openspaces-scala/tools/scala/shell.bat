22d1cde6b6c14cfe228a2caf5410e28d4a064709
Niv Ingberg <niv@gigaspaces.com>
2016-02-15 17:46:07 +0200

    GS-12745 - Removed redundant options Removed RMI options which were set to their default values, and merged the remaining with XAP_OPTIONS

--- openspaces-scala/tools/scala/shell.bat
+++ openspaces-scala/tools/scala/shell.bat
@@ -1,7 +1,7 @@
 @call "%~dp0\..\..\bin\setenv.bat"
 
 set REPL_CLASSPATH=-cp %GS_JARS%;%SPRING_JARS%;%SIGAR_JARS%;"%XAP_HOME%\lib\platform\scala\lib\*;";"%XAP_HOME%\lib\platform\scala\*;"
-set REPL_JAVA_OPTS=%JAVA_OPTIONS% %RMI_OPTIONS% %XAP_OPTIONS%
+set REPL_JAVA_OPTS=%JAVA_OPTIONS% %XAP_OPTIONS%
 set REPL_MAIN_CLASS=org.openspaces.scala.repl.GigaSpacesScalaRepl
 set REPL_COMPILER_OPTS=-usejavacp -Yrepl-sync
 set REPL_COMMAND=%JAVACMD% %REPL_JAVA_OPTS% %REPL_CLASSPATH% %REPL_MAIN_CLASS% %REPL_COMPILER_OPTS%
