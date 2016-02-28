0b8eeb30550d7409400006371c9636f758c1b680
Niv Ingberg <niv@gigaspaces.com>
2016-02-28 09:50:59 +0200

    GS-12745 - renamed service grid env vars to XAP_

--- openspaces-scala/tools/scala/shell.bat
+++ openspaces-scala/tools/scala/shell.bat
@@ -1,9 +1,8 @@
 @call "%~dp0\..\..\bin\setenv.bat"
 
 set REPL_CLASSPATH=-cp %GS_JARS%;%SPRING_JARS%;%SIGAR_JARS%;"%XAP_HOME%\lib\platform\scala\lib\*;";"%XAP_HOME%\lib\platform\scala\*;"
-set REPL_JAVA_OPTS=%JAVA_OPTIONS% %XAP_OPTIONS%
 set REPL_MAIN_CLASS=org.openspaces.scala.repl.GigaSpacesScalaRepl
 set REPL_COMPILER_OPTS=-usejavacp -Yrepl-sync
-set REPL_COMMAND=%JAVACMD% %REPL_JAVA_OPTS% %REPL_CLASSPATH% %REPL_MAIN_CLASS% %REPL_COMPILER_OPTS%
+set REPL_COMMAND=%JAVACMD% %XAP_OPTIONS% %REPL_CLASSPATH% %REPL_MAIN_CLASS% %REPL_COMPILER_OPTS%
 
 %REPL_COMMAND%
--- openspaces-scala/tools/scala/shell.sh
+++ openspaces-scala/tools/scala/shell.sh
@@ -2,9 +2,8 @@ export JSHOMEDIR=`dirname $0`/../..
 . `dirname $0`/../../bin/setenv.sh
 
 REPL_CLASSPATH="-cp $GS_JARS:$SPRING_JARS:$SIGAR_JARS:"${XAP_HOME}"/lib/platform/scala/lib/*:"${XAP_HOME}"/lib/platform/scala/*"
-REPL_JAVA_OPTS="$JAVA_OPTIONS $XAP_OPTIONS"
 REPL_MAIN_CLASS="org.openspaces.scala.repl.GigaSpacesScalaRepl"
 REPL_COMPILER_OPTS="-usejavacp -Yrepl-sync"
-REPL_COMMAND="$JAVACMD $REPL_JAVA_OPTS $REPL_CLASSPATH $REPL_MAIN_CLASS $REPL_COMPILER_OPTS"
+REPL_COMMAND="$JAVACMD $XAP_OPTIONS $REPL_CLASSPATH $REPL_MAIN_CLASS $REPL_COMPILER_OPTS"
 
 $REPL_COMMAND
